5ba4d926a331c7bf54c628d27caf47c228697105
ITDevv <aidonline01@gmail.com>
2021-11-20 20:20:17 +0000

    added readme

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Install and run
+1. `git clone https://github.com/AidOnline01/preact-vite-ssr.git`
+2. Install dependencies: `npm i`
+3. Run it: `npm run dev`
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import preact from "@preact/preset-vite";
 import ssr from "vite-plugin-ssr/plugin";
 
 export default {
-  server: {
-    port: 7503,
-    hmr: {
-      path: 'wss',
-      protocol: 'wss',
-      clientPort: 7503,
-      port: 7503
-    }
-  },
   plugins: [preact(), ssr()],
   resolve: {
     alias: {
