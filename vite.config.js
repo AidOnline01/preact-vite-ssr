86d03d772138dcf45d1200c97c896e074d33c5a1
ITDevv <aidonline01@gmail.com>
2021-11-20 19:18:29 +0000

    React ssr

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 
 export default {
+  server: {
+    port: 7503,
+    hmr: {
+      path: 'wss',
+      protocol: 'wss',
+      clientPort: 7503,
+      port: 7503
+    }
+  },
   plugins: [react(), ssr()],
 };
